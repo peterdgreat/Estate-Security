25b4190cba469d9d0d38dc1de7e69b3ab1d4ba7c
Peter Ajayi <peterdgreat2016@gmail.com>
2024-04-25 23:19:02 +0100

    users(family members) and mailer added

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  belongs_to :family, class_name: "family", foreign_key: "family_id",optional: true
+end
